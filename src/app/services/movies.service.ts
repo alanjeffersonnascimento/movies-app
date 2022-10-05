0fed1b3e48b393910ee2b4b1fafecdf60042152c
Nascimento Alan Jefferson <AlanJefferson.Nascimento@wexinc.com>
2022-10-05 15:43:05 -0300

    Adding Home Page Slide Banner and Sections

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MovieDto } from '../models/movie';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  baseUrl: string = 'https://api.themoviedb.org/3';
+  apiKey: string = '8c247ea0b4b56ed2ff7d41c9a833aa77';
+
   constructor(private http: HttpClient) {}
 
-  getMovies() {
-    return this.http.get('https://api.themoviedb.org/3/movie/upcoming?api_key=8c247ea0b4b56ed2ff7d41c9a833aa77');
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
   }
 }
